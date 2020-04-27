18735afbf17dffe163e687231552f28cd7f9e930
DHvyas <vyasdhruvh@gmail.com>
2020-04-27 17:40:31 +0530

    Added Home Page

---
+++ project/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+            <div class="content">
+                <p>Footer</p>
+            </div>
+        </div>
+    </footer>
+</template>
+<style>
+footer{
+    background-color: #00BFA6!important;
+}
+</style>
+<script>
+export default {
+  name: 'Footer1'
+}
+</script>
---
+++ project/src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <b-navbar>
+            <template slot="brand">
+                <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                    <a style="background-color:none!important; font-weight:600;" class="is-primary" >TuraConnect</a>
+                </b-navbar-item>
+            </template>
+            <template slot="start">
+                <b-navbar-item href="#">
+                    Home
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Documentation
+                </b-navbar-item>
+                <b-navbar-dropdown label="Info">
+                    <b-navbar-item href="#">
+                        About
+                    </b-navbar-item>
+                    <b-navbar-item href="#">
+                        Contact
+                    </b-navbar-item>
+                </b-navbar-dropdown>
+            </template>
+
+            <template slot="end">
+                <b-navbar-item tag="div">
+                    <div class="buttons">
+                        <a class="button is-primary">
+                            <strong>Sign up</strong>
+                        </a>
+                        <a class="button is-light">
+                            Log in
+                        </a>
+                    </div>
+                </b-navbar-item>
+            </template>
+        </b-navbar>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
